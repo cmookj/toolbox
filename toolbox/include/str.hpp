37c16c3706449fed896a5fce8cae94da679d2365
Changmook Chun <cmookj@duck.com>
2025-09-05 01:21:54 -0700

    KMP algorithm

--- toolbox/include/str.hpp
+++ toolbox/include/str.hpp
@@ -4,19 +4,24 @@
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace gpw::str {
 
-std::string_view ltrim (std::string_view str);
-std::string_view rtrim (std::string_view str);
-std::string_view trim (std::string_view str);
+std::string_view
+ltrim (std::string_view str);
+std::string_view
+rtrim (std::string_view str);
+std::string_view
+trim (std::string_view str);
 
 std::string
 trim (const std::string& str, const std::string& whitespace = " \t");
 
-std::string reduce (
+std::string
+reduce (
     const std::string& str,
-    const std::string& fill = " ",
+    const std::string& fill       = " ",
     const std::string& whitespace = " \t"
 );
 
@@ -25,27 +30,28 @@ std::string to_lower (std::string);
 std::string remove_illegal_char (std::string);
 
 // Compare two strings case insensitive. If strings are identical, return zero.
-int compare (std::string str1, std::string str2);
+int
+compare (std::string str1, std::string str2);
 
 // Formatting string
 template <typename... Args>
-std::string format_string (const std::string& format, Args... args) {
-    size_t size = std::snprintf (nullptr, 0, format.c_str(), args...) +
-                  1; // Extra space for '\0'
+std::string
+format_string (const std::string& format, Args... args) {
+    size_t size = std::snprintf (nullptr, 0, format.c_str(), args...) + 1;  // Extra space for '\0'
     if (size <= 0) {
         throw std::runtime_error ("Invalid string format");
     }
     std::unique_ptr<char[]> buf (new char[size]);
     std::snprintf (buf.get(), size, format.c_str(), args...);
-    return std::string (
-        buf.get(), buf.get() + size - 1
-    ); // We don't want the '\0' inside
+    return std::string (buf.get(), buf.get() + size - 1);  // We don't want the '\0' inside
 }
 
-std::string format (const char*);
+std::string
+format (const char*);
 
 template <typename T, typename... Args>
-std::string format (const char* s, T value, Args... args) {
+std::string
+format (const char* s, T value, Args... args) {
     std::stringstream ss;
 
     while (s && *s) {
@@ -59,9 +65,7 @@ std::string format (const char* s, T value, Args... args) {
                 ss << '{';
                 s += 2;
             } else
-                throw std::runtime_error {
-                    "invalid format: { should be followed by another { or }"
-                };
+                throw std::runtime_error{"invalid format: { should be followed by another { or }"};
 
             break;
 
@@ -69,18 +73,21 @@ std::string format (const char* s, T value, Args... args) {
             if (*(s + 1) == '}') {
                 ss << '}';
                 s += 2;
-            } else
-                throw std::runtime_error {
-                    "invalid format: } should be followed by another }"
-                };
+            } else throw std::runtime_error{"invalid format: } should be followed by another }"};
 
             break;
 
-        default:
-            ss << *s++;
+        default: ss << *s++;
         }
     }
-    throw std::runtime_error {"extra arguments provided to format"};
+    throw std::runtime_error{"extra arguments provided to format"};
 }
 
-} // namespace gpw::str
+// KMP (Knuth-Morris-Pratt) search algorithm
+std::vector<int>
+llps (std::string& pat);
+
+std::vector<int>
+search (std::string& pat, std::string& txt);
+
+}  // namespace gpw::str
--- toolbox/src/str.cpp
+++ toolbox/src/str.cpp
@@ -14,74 +14,72 @@ const std::string illegal_chars = "\\/";
 #error "Unknown compiler"
 #endif
 
-std::string_view ltrim (std::string_view str) {
+std::string_view
+ltrim (std::string_view str) {
     const auto pos (str.find_first_not_of (" \t\n\r\f\v"));
     str.remove_prefix (std::min (pos, str.length()));
     return str;
 }
 
-std::string_view rtrim (std::string_view str) {
+std::string_view
+rtrim (std::string_view str) {
     const auto pos (str.find_last_not_of (" \t\n\r\f\v"));
     str.remove_suffix (std::min (str.length() - pos - 1, str.length()));
     return str;
 }
 
-std::string_view trim (std::string_view str) {
+std::string_view
+trim (std::string_view str) {
     str = ltrim (str);
     str = rtrim (str);
     return str;
 }
 
-std::string trim (const std::string& str, const std::string& whitespace) {
+std::string
+trim (const std::string& str, const std::string& whitespace) {
     const auto begin = str.find_first_not_of (whitespace);
-    if (begin == std::string::npos)
-        return ""; // no content
+    if (begin == std::string::npos) return "";  // no content
 
-    const auto end = str.find_last_not_of (whitespace);
+    const auto end   = str.find_last_not_of (whitespace);
     const auto range = end - begin + 1;
 
     return str.substr (begin, range);
 }
 
-std::string reduce (
-    const std::string& str,
-    const std::string& fill,
-    const std::string& whitespace
-) {
+std::string
+reduce (const std::string& str, const std::string& fill, const std::string& whitespace) {
     // trim first
     auto result = trim (str, whitespace);
 
     // replace sub ranges
     auto begin_space = result.find_first_of (whitespace);
     while (begin_space != std::string::npos) {
-        const auto end_space =
-            result.find_first_not_of (whitespace, begin_space);
-        const auto range = end_space - begin_space;
+        const auto end_space = result.find_first_not_of (whitespace, begin_space);
+        const auto range     = end_space - begin_space;
 
         result.replace (begin_space, range, fill);
 
         const auto new_begin = begin_space + fill.length();
-        begin_space = result.find_first_of (whitespace, new_begin);
+        begin_space          = result.find_first_of (whitespace, new_begin);
     }
 
     return result;
 }
 
-std::string to_upper (std::string source_str) {
-    std::transform (
-        source_str.begin(), source_str.end(), source_str.begin(), ::toupper
-    );
+std::string
+to_upper (std::string source_str) {
+    std::transform (source_str.begin(), source_str.end(), source_str.begin(), ::toupper);
     return source_str;
 }
 
-std::string to_lower (std::string source_str) {
-    std::transform (
-        source_str.begin(), source_str.end(), source_str.begin(), ::tolower
-    );
+std::string
+to_lower (std::string source_str) {
+    std::transform (source_str.begin(), source_str.end(), source_str.begin(), ::tolower);
     return source_str;
 }
 
-std::string remove_illegal_char (std::string str) {
+std::string
+remove_illegal_char (std::string str) {
     for (std::string::iterator it = str.begin(); it < str.end(); ++it) {
         bool found = illegal_chars.find (*it) != std::string::npos;
         if (found) {
@@ -91,27 +89,25 @@ std::string remove_illegal_char (std::string str) {
     return str;
 }
 
-int compare (std::string str1, std::string str2) {
+int
+compare (std::string str1, std::string str2) {
     return to_lower (str1).compare (to_lower (str2));
 }
 
-std::string format (const char* s) {
-    if (s == nullptr)
-        return std::string {};
+std::string
+format (const char* s) {
+    if (s == nullptr) return std::string{};
 
     std::stringstream ss;
     while (*s) {
         switch (*s) {
         case '{':
-            if (*(s + 1) == '}')
-                throw std::runtime_error {"invalid format: missing arguments"};
+            if (*(s + 1) == '}') throw std::runtime_error{"invalid format: missing arguments"};
             else if (*(s + 1) == '{') {
                 ss << '{';
                 s += 2;
             } else
-                throw std::runtime_error {
-                    "invalid format: { should be followed by another { or }"
-                };
+                throw std::runtime_error{"invalid format: { should be followed by another { or }"};
 
             break;
 
@@ -119,18 +115,88 @@ std::string format (const char* s) {
             if (*(s + 1) == '}') {
                 ss << '}';
                 s += 2;
-            } else
-                throw std::runtime_error {
-                    "invalid format: } should be followed by another }"
-                };
+            } else throw std::runtime_error{"invalid format: } should be followed by another }"};
 
             break;
 
-        default:
-            ss << *s++;
+        default: ss << *s++;
         }
     }
     return ss.str();
 }
 
-} // namespace gpw::str
+// KMP (Knuth-Morris-Pratt) search algorithm
+std::vector<int>
+llps (std::string& pat) {
+    // The length of longest proper prefix which is also a suffix for the previous index.
+    int len = 0;
+
+    std::vector<int> _llps (pat.length());
+    // llps[0] is always 0
+    _llps[0] = 0;
+
+    int i = 1;
+    while (i < pat.length()) {
+        // If characters match, increment the size of lps
+        if (pat[i] == pat[len]) {
+            len++;
+            _llps[i] = len;
+            i++;
+        }
+
+        // If there is a mismatch
+        else {
+            if (len != 0) {
+                // Update len to the previous lps value
+                // to avoid reduntant comparisons
+                len = _llps[len - 1];
+            } else {
+                // If no matching prefix found, set lps[i] to 0
+                _llps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return _llps;
+}
+
+std::vector<int>
+search (std::string& pat, std::string& txt) {
+    int n = txt.length();
+    int m = pat.length();
+
+    std::vector<int> _llps = llps (pat);
+    std::vector<int> res;
+
+    // Pointers i and j, for traversing
+    // the text and pattern
+    int i = 0;
+    int j = 0;
+
+    while (i < n) {
+        // If characters match, move both pointers forward
+        if (txt[i] == pat[j]) {
+            i++;
+            j++;
+
+            // If the entire pattern is matched store the start index in result
+            if (j == m) {
+                res.push_back (i - j);
+
+                // Use LPS of previous index to skip unnecessary comparisons
+                j = _llps[j - 1];
+            }
+        }
+
+        // If there is a mismatch
+        else {
+            // Use lps value of previous index to avoid redundant comparisons
+            if (j != 0) j = _llps[j - 1];
+            else i++;
+        }
+    }
+    return res;
+}
+
+}  // namespace gpw::str
